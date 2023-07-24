df8c3a789eb85211134e71bc14bd4d67e15802a2
Lupin <skorpion_94@list.ru>
2023-07-24 16:16:39 +0200

    My 3.2 attempt

--- Do_it.c
+++ Do_it.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Массивы распадаются на указатели при переходе к функции.
-Используя только указатель, вы не можете получить размер массива.
-Вы должны передать вызывающей функции еще один аргумент,
-который является размером массива. Нужно пояснение...*/
 
-int findSizeOutputArray(int size_input_array, int input_array[])
+int get_count_even_numbers(int input_array_size, int input_array[])
 {
     int count_numbers = 0;
-    for (int i = 0; i < size_input_array; i++)
+    for (int i = 0; i < input_array_size; i++)
     {
         if (input_array[i] % 2 == 0)
         {
@@ -19,16 +15,14 @@ int findSizeOutputArray(int size_input_array, int input_array[])
     return count_numbers;
 }
 
-int fillingOutputArray(int size_input_array, int size_output_array, int input_array[], int *output_array)
+int* filter_Even(int input_array_size, int size_output_array, int input_array[], int *output_array)
 {
     int p = 0;
-    // int *output_array = malloc(size_output_array * sizeof(int));// веделение памяти под size_output_array колличество элементов
-    for (int i = 0; i < size_input_array; i++)
+    for (int i = 0; i < input_array_size; i++)
     {
         if (input_array[i] % 2 == 0)
         {
             output_array[p] = input_array[i];
-            // printf("%d , ",output_array[p]);
             p++;
         }
     }
@@ -40,23 +34,18 @@ int fillingOutputArray(int size_input_array, int size_output_array, int input_ar
     printf("]\n");
 
     return (int *)output_array; // typecast
-    // free(output_array); --типо освобождение памяти
 }
 
 int main()
 {
     int input_array[] = {6, 1, 4, 9, 2};
 
-    int size_input_array = sizeof(input_array) / sizeof(input_array[0]);
+    int input_array_size = sizeof(input_array) / sizeof(input_array[0]);
 
-    int size_output_array = findSizeOutputArray(size_input_array, input_array);
-    int *output_array = malloc(size_output_array * sizeof(int));
+    int size_output_array = get_count_even_numbers(input_array_size, input_array);
+    int *output_array = malloc(size_output_array * sizeof(int)); //веделение памяти | free(output_array); --освобождение памяти
 
-    // printf("This array size: %d",sizeArr2);
-
-    // fillingOutputArray(sizeArr2,input_array);
-
-    int *p = fillingOutputArray(size_input_array, size_output_array, input_array, output_array);
+    int *p = filter_Even(input_array_size, size_output_array, input_array, output_array);
     printf("p[0] = %d\n", p[0]);
     printf("p[1] = %d\n", p[1]);
     printf("p[2] = %d\n", p[2]);
--- FirstLesson.c
+++ FirstLesson.c
@@ -1,83 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int findArraySize(int *arr[])
-{
-    int size = sizeof(arr)/sizeof(arr[0]);
-    return size;
-}
-
-void printArray(int arr[])
-{
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ,",arr[i]);
-    } 
-    return;
-}
-
-// int searchEvenNumber (int array[])
-// {
-
-//     for (int i = 0; i < 5; i++)
-//     {
-//         if (array[i] % 2 == 0)
-//         {
-//             int number = array[i];
-//             printf("%d ,\n",array[i]);
-//             return number;
-//         }
-        
-//     }
-// }
-/*
-Возвращение указателя на массив (на начало массива). 
-Возвращаемый указатель не может указывать на автоматический массив,
-объявленный внутри этой функции,
-так как такой массив будет уничтожен при выходе из функции.
-*/
-int* createArray(int arr[]){ 
-    int j = 0;
-    int arr2[5];
-    for (int i = 0; i < 5; i++)
-    {
-        if (arr[i] % 2 == 0)
-        {
-
-            arr2[j] = arr[i];
-            printf("%d ,",arr2[j]);
-            j++;
-
-        }
-    }
-    return &arr2;
-    //return *((int*)arr2);
-}
+#define Action_arif(a,b) a*b;
 
 int main(){
-
-    int arr1[5] = {6,1,4,9,2}; 
-    int size = findArraySize(arr1);
-    printf("%d", size)
-    //int arr2[3] = createArray(arr1);
+    var1
 
 
-    // int j = 1;
-    // int arr2[j];
-
-    // int size = sizeof(arr1)/sizeof(arr1[0]);
-    // //printf("%d",size);
-    
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if (arr1[i] % 2 == 0)
-    //     {
-    //         arr2[j] = arr1[i];
-    //         printf("%d ,",arr2[j]);
-    //         j++;
-    //     }
-    // }
-
-    //printArray(arr1);
-    //searchEvenNumber(arr1);
 }
