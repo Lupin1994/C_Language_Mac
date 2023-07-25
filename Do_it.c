b01e662a39807def3699afc90e726090bc508d62
Lupin <skorpion_94@list.ru>
2023-07-25 12:44:45 +0200

    My 3.3 attempt

--- Do_it.c
+++ Do_it.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 
 
-int get_count_even_numbers(int input_array_size, int input_array[])
-{
-    int count_numbers = 0;
-    for (int i = 0; i < input_array_size; i++)
-    {
-        if (input_array[i] % 2 == 0)
-        {
-            count_numbers++;
-        }
-    }
-    return count_numbers;
-}
-
-int* filter_Even(int input_array_size, int size_output_array, int input_array[], int *output_array)
+int* filter_Even(int input_array_size, int input_array[], int *output_array)
 {
     int p = 0;
     for (int i = 0; i < input_array_size; i++)
@@ -27,7 +14,7 @@ int* filter_Even(int input_array_size, int size_output_array, int input_array[],
         }
     }
     printf("[");
-    for (int i = 0; i < size_output_array; i++)
+    for (int i = 0; i < input_array_size; i++)
     {
         printf("%d ", output_array[i]);
     }
@@ -42,10 +29,9 @@ int main()
 
     int input_array_size = sizeof(input_array) / sizeof(input_array[0]);
 
-    int size_output_array = get_count_even_numbers(input_array_size, input_array);
-    int *output_array = malloc(size_output_array * sizeof(int)); //веделение памяти | free(output_array); --освобождение памяти
+    int *output_array = malloc(input_array_size * sizeof(int)); //веделение памяти | free(output_array); --освобождение памяти
 
-    int *p = filter_Even(input_array_size, size_output_array, input_array, output_array);
+    int *p = filter_Even(input_array_size, input_array, output_array);
     printf("p[0] = %d\n", p[0]);
     printf("p[1] = %d\n", p[1]);
     printf("p[2] = %d\n", p[2]);
