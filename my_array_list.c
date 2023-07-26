68d3c4513058611d83a8ecf40600503e380e651b
Lupin <skorpion_94@list.ru>
2023-07-26 14:40:05 +0200

    first_try

--- my_array_list.c
+++ my_array_list.c
@@ -3,17 +3,31 @@
 #include <conio.h>
 #include <stdbool.h>
 
+void *print_Array(int *array,int array_size) {
+
+    printf("[");
+    for (int i = 0; i < array_size; i++) {
+        printf("%d\t",array[i]);
+    }
+    printf("]\n");
+
+    return 0;
+}
 
 void my_array_list(){
     int size_array = 2;
     int count_add_numbers = 0;
-    int first_array[size_array];
+    
+    int *first_array = malloc(size_array * sizeof(int));
+    int *second_array;
 
-    // char stop_char;
-    // char end = 's';
+    char stop_char;
+    char end = 's';
     // printf("If you want stop push -s\n");
     // scanf("%c");
-    while (true)
+    bool flag = true;
+
+    while (flag == true)
     {
         
         if(size_array >= count_add_numbers){
@@ -21,58 +35,27 @@ void my_array_list(){
             {
                 printf("Write element number - %d : ",count_add_numbers+1);
                 scanf("%d",&first_array[i]);
-                if (first_array[i] == -1)
-                {
-                    return false;
-                }
                 printf("\n element = %d \n",first_array[i]);
                 count_add_numbers++;
             }
         }
         else{
-            size_array = size_array*2;
-            int second_array[size_array];
+            size_array *= 2;
+            int *second_array = malloc(size_array * sizeof(int));
             for (int i = 0; i < size_array; i++)
             {
                 second_array[i] = first_array[i];
             }
-            if(size_array >= count_add_numbers){
-                for (int i = 0; i < size_array; i++)
-                {
-                    printf("Write element number - %d : ",count_add_numbers+1);
-                    scanf("%d",&second_array[i]);
-                    if (first_array[i] == -1)
-                    {
-                        return false;
-                    }
-                    printf("\n element = %d \n",second_array[i]);
-                    count_add_numbers++;
-                }
-            }
-            else
-            {
-                size_array = size_array*2;
-                for (int i = 0; i < size_array; i++)
-                {
-                    first_array[i] = second_array[i];
-                }
-                if(size_array >= count_add_numbers){
-                    for (int i = 0; i < size_array; i++)
-                    {
-                        printf("Write element number - %d : ",count_add_numbers+1);
-                        scanf("%d",&first_array[i]);
-                        if (first_array[i] == -1)
-                        {
-                            return false;
-                        }
-                        printf("\n element = %d \n",first_array[i]);
-                        count_add_numbers++;
-                    }
-                }
-            }
+            *first_array = &second_array;
         }
+        printf("[");
+        for (int i = 0; i < size_array; i++) {
+            printf("%d\t",first_array[i]);
+        }
+        printf("]\n");
+        printf("If you want stop push -s :");
+        flag = scanf("%c",&stop_char) == end ? false:true;
     }
-    return 0;
 }
 
 int main()
