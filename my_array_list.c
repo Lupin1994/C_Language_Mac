28e16dd1fcd7fce7d5fb1971813f811585575f21
Lupin <skorpion_94@list.ru>
2023-07-27 16:36:18 +0200

    Day number-2.1

--- my_array_list.c
+++ my_array_list.c
@@ -13,14 +13,44 @@ void *print_Array(int *array,int array_size) {
 
     return 0;
 }
+int* filter_Even(int input_array_size, int size_output_array, int input_array[], int *output_array)
+{
+    int p = 0;
+    for (int i = 0; i < input_array_size; i++)
+    {
+        if (input_array[i] % 2 == 0)
+        {
+            output_array[p] = input_array[i];
+            p++;
+        }
+    }
+    printf("[");
+    for (int i = 0; i < size_output_array; i++)
+    {
+        printf("%d ", output_array[i]);
+    }
+    printf("]\n");
+
+    return (int *)output_array; // typecast
+}
+typedef struct input_array First_array; // Определение нового типа
+
+struct input_array // Создание структуры входного массива
+{
+    int size_array;
+    int count_add_numbers;
+    int *start_input_array;
+};
+
 
 void my_array_list(){
-    int size_array = 2;
-    int count_add_numbers = 0;
-    
-    int *first_array = malloc(size_array * sizeof(int));
+    int _size_array = 2;
+    int _count_add_numbers = 0;
+    First_array first_array = {.size_array = _size_array, .count_add_numbers = _count_add_numbers,
+    .start_input_array = malloc(sizeof(int[_size_array]))};
+    //int *first_array = malloc(size_array * sizeof(int));
     int *second_array;
-
+    
     char stop_char;
     char end = 's';
     // printf("If you want stop push -s\n");
@@ -30,40 +60,40 @@ void my_array_list(){
     while (flag == true)
     {
         
-        if(size_array > count_add_numbers){
-            for (int i = count_add_numbers; i <= size_array; i++)
+        if(_size_array > _count_add_numbers){
+            for (int i = _count_add_numbers; i <= _size_array; i++)
             {
                 printf("[");
-                    for (int i = 0; i < count_add_numbers; i++) {
-                        printf("%d\t",first_array[i]);
+                    for (int i = 0; i < _count_add_numbers; i++) {
+                        printf("%d\t",first_array.start_input_array[i]);
                     }
                     printf("]\n");
-                printf("\nWrite element number - %d : ",count_add_numbers+1);
-                scanf("%d",&first_array[i]);
-                printf("\n element = %d \n",first_array[i]);
+                printf("\nWrite element number - %d : ",_count_add_numbers+1);
+                scanf("%d",&first_array.start_input_array[i]);
+                printf("\n element = %d \n",first_array.start_input_array[i]);
                 printf("If you want stop push -s :\n");
                 scanf("%c",&stop_char);
                 if (stop_char == end)
                 {       
                     flag =false;
                     printf("[");
-                    for (int i = 0; i < count_add_numbers; i++) {
-                        printf("%d\t",first_array[i]);
+                    for (int i = 0; i < _count_add_numbers; i++) {
+                        printf("%d\t",first_array.start_input_array[i]);
                     }
                     printf("]\n");
                     break;
                 }
-                count_add_numbers++;
+                _count_add_numbers++;
             }
         }
         else{
-            size_array = size_array * 2;
-            int *second_array = malloc(size_array * sizeof(int));
-            for (int i = 0; i <count_add_numbers;i++)
+            _size_array = _size_array * 2;
+            int *second_array = malloc(_size_array * sizeof(int));
+            for (int i = 0; i <_count_add_numbers;i++)
             {
-                second_array[i] = first_array[i];
+                second_array[i] = first_array.start_input_array[i];
             }
-            first_array = second_array;
+            first_array.start_input_array = second_array;
         }
     }
 }
@@ -71,4 +101,6 @@ void my_array_list(){
 int main()
 {
     my_array_list();
+
+    //print_Array();
 }
