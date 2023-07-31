77235439e5fc71189508ccdbf254f6be382eb2f1
Lupin <skorpion_94@list.ru>
2023-07-31 20:25:30 +0200

    Day 4.1

--- List.c
+++ List.c
@@ -13,11 +13,11 @@ struct List // Создание структуры входного массив
     int *_start_input_array;
 };
 
-void* list_print(TList list) {
+void* list_print(TList *list) {
     printf("[");
-    for (int i = 0; i < list.count_add_numbers; i++)
+    for (int i = 0; i < list->count_add_numbers; i++)
     {
-        printf("%d", list._start_input_array[i]);
+        printf("%d,\t", list->_start_input_array[i]);
     }
     printf("]\n");
 }
@@ -31,18 +31,18 @@ TList list_create() {
     return list;
 }
 
-void* list_add(TList list, int item) { // Добавление одного элемента
-    if (list._size_array <= list.count_add_numbers)
+void* list_add(TList *list, int item) { // Добавление одного элемента
+    if (list->_size_array <= list->count_add_numbers)
     {
-        list._size_array *= 2;
+        list->_size_array *= 2;
     }
     
-    list._start_input_array[list.count_add_numbers] = item;
-    list.count_add_numbers++;
+    list->_start_input_array[list->count_add_numbers] = item;
+    list->count_add_numbers++;
 }
 
-int list_get_length(TList list) { // Через каунтер
-int list_length = list.count_add_numbers;
+int list_get_length(TList *list) { // Через каунтер
+int list_length = list->count_add_numbers;
 return list_length;
 }
 
@@ -52,7 +52,11 @@ int main()
     TList list = list_create();
     int item = 5;
     
-    list_add(list,item);
-    list_print(list);
+    list_add(&list,item);
+    list_add(&list,item);
+    list_add(&list,item);
+    list_add(&list,item);
+
+    list_print(&list);
     printf("%d",list.count_add_numbers);
 }
