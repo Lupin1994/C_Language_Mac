c8de0232bb4ce20a225bbe4897237f8e016714dd
Lupin <skorpion_94@list.ru>
2023-07-31 20:17:39 +0200

    Day 4.1

--- List.c
+++ List.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <stdbool.h>
-#include <limits.h>
 
 typedef struct List TList; // Определение нового типа
 
@@ -10,37 +9,40 @@ struct List // Создание структуры входного массив
 {
     int count_add_numbers;
 
-    int _count_call_function;
+    int _size_array;
     int *_start_input_array;
 };
 
-void* list_print(TList *list, int list_size) {
+void* list_print(TList list) {
     printf("[");
-    for (int i = 0; i < list_size; i++)
+    for (int i = 0; i < list.count_add_numbers; i++)
     {
-        printf("%d", list[i]);
+        printf("%d", list._start_input_array[i]);
     }
     printf("]\n");
 }
 
 TList list_create() {
     TList list = {
-        ._count_call_function = 0,
+        ._size_array = 1,
         .count_add_numbers = 0,
-        ._start_input_array = realloc(list._start_input_array, (list.count_add_numbers + 1) * sizeof(int))
+        ._start_input_array = malloc(sizeof(int[list._size_array]))
     };
     return list;
 }
 
-void* list_add(TList *list, int item) { // Добавление одного элемента
-
-    list->_start_input_array[list->count_add_numbers] = item;
-    list->count_add_numbers++;
-    list->_count_call_function++;
+void* list_add(TList list, int item) { // Добавление одного элемента
+    if (list._size_array <= list.count_add_numbers)
+    {
+        list._size_array *= 2;
+    }
+    
+    list._start_input_array[list.count_add_numbers] = item;
+    list.count_add_numbers++;
 }
 
-int list_get_length(TList *list) { // Через каунтер
-int list_length = list->_count_call_function;
+int list_get_length(TList list) { // Через каунтер
+int list_length = list.count_add_numbers;
 return list_length;
 }
 
@@ -50,7 +52,7 @@ int main()
     TList list = list_create();
     int item = 5;
     
-    list_add(list._start_input_array,item);
-    list_print(list._start_input_array,list.count_add_numbers);
+    list_add(list,item);
+    list_print(list);
     printf("%d",list.count_add_numbers);
 }
