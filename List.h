02b9a17e5f54c6015894b55757265fb2b7bbad07
Lupin <skorpion_94@list.ru>
2023-08-01 15:58:56 +0200

    Day 5.1

---
+++ List.h
@@ -0,0 +1,14 @@
+typedef struct List TList; // Определение нового типа
+
+struct List // Создание структуры входного массива
+{
+    int count_add_numbers;
+
+    int _size_array;
+    int *_start_input_array;
+};
+
+void* list_print(TList *list);
+TList list_create();
+void* list_add(TList *list, int item);
+int list_get_length(TList *list);
--- main.c
+++ main.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <stdbool.h>
-typedef struct List TList; // Определение нового типа
-
-struct List // Создание структуры входного массива
-{
-    int count_add_numbers;
-
-    int _size_array;
-    int *_start_input_array;
-};
-
-void* list_print(TList *list);
-TList list_create();
-void* list_add(TList *list, int item);
-int list_get_length(TList *list);
+#include "List.h"
 
 int main()
 {
