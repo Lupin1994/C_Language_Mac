ccd5fe7300244675c078d953e0b9cb61479af413
Lupin <skorpion_94@list.ru>
2023-08-01 16:13:04 +0200

    Day 5.1

--- List.c
+++ List.c
@@ -26,18 +26,18 @@ TList list_create() {
     TList list = {
         ._size_array = 1,
         .count_add_numbers = 0,
-        //._start_input_array = malloc(sizeof(int[list._size_array]))
-        ._start_input_array = NULL
+        ._start_input_array = malloc(sizeof(int[list._size_array]))
+        //._start_input_array = NULL
     };
-    list._start_input_array = (int*)realloc(list._start_input_array,(list.count_add_numbers + 1) * sizeof(int) );
+    //list._start_input_array = (int*)realloc(list._start_input_array,(list.count_add_numbers + 1) * sizeof(int) );
     return list;
 }
 
 void* list_add(TList *list, int item) { // Добавление одного элемента
-    // if (list->_size_array <= list->count_add_numbers)
-    // {
-    //     list->_size_array *= 2;
-    // }
+    if (list->_size_array <= list->count_add_numbers)
+    {
+        list->_size_array *= 2;
+    }
     
     list->_start_input_array[list->count_add_numbers] = item;
     list->count_add_numbers++;
